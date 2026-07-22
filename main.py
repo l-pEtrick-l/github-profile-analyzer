cd32d910f5d01e03625cf326f5d044a8148a4a60
Bryan Petrick <www.bryanpetrick@gmail.com>
2026-07-22 15:12:29 -0300

    Improve terminal interface using Rich

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
 from github_api import buscar_usuario
+from rich.console import Console
+from rich.table import Table
+from rich.panel import Panel
 
-usuario = input("Digite um usuário do GitHub: ")
+console = Console()
+
+console.print(
+    Panel.fit(
+        "[bold cyan]GitHub Profile Analyzer[/bold cyan]",
+        border_style="blue"
+    )
+)
+
+usuario = console.input("[bold green]Digite um usuário do GitHub:[/bold green] ")
 
 dados = buscar_usuario(usuario)
 
 if dados:
 
-    print("=" * 50)
-    print("        GITHUB PROFILE ANALYZER")
-    print("=" * 50)
+    tabela = Table(title="GitHub Profile")
 
-    print(f"Nome: {dados['name']}")
-    print(f"Login: {dados['login']}")
-    print(f"Bio: {dados['bio']}")
-    print(f"Empresa: {dados['company']}")
-    print(f"Localização: {dados['location']}")
-    print(f"Blog/Site: {dados['blog']}")
-    print(f"Email Público: {dados['email']}")
-    print(f"Twitter: {dados['twitter_username']}")
-    print(f"Seguidores: {dados['followers']}")
-    print(f"Seguindo: {dados['following']}")
-    print(f"Repositórios Públicos: {dados['public_repos']}")
-    print(f"Gists Públicos: {dados['public_gists']}")
-    print(f"Criado em: {dados['created_at']}")
-    print(f"Última atualização: {dados['updated_at']}")
-    print(f"Avatar: {dados['avatar_url']}")
-    print(f"Perfil: {dados['html_url']}")
+    tabela.add_column("Campo", style="cyan", no_wrap=True)
+    tabela.add_column("Informação", style="white")
 
-    print("=" * 50)
+    tabela.add_row("Nome", str(dados["name"]))
+    tabela.add_row("Login", str(dados["login"]))
+    tabela.add_row("Bio", str(dados["bio"]))
+    tabela.add_row("Empresa", str(dados["company"]))
+    tabela.add_row("Localização", str(dados["location"]))
+    tabela.add_row("Seguidores", str(dados["followers"]))
+    tabela.add_row("Seguindo", str(dados["following"]))
+    tabela.add_row("Repositórios", str(dados["public_repos"]))
+    tabela.add_row("Gists", str(dados["public_gists"]))
+    tabela.add_row("Criado em", str(dados["created_at"][:10]))
+    tabela.add_row("Atualizado", str(dados["updated_at"][:10]))
+    tabela.add_row("Perfil", str(dados["html_url"]))
+
+    console.print(tabela)
 
 else:
-    print("Usuário não encontrado.")
+
+    console.print("[bold red]Usuário não encontrado.[/bold red]")
